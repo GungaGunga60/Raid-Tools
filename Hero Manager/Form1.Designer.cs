d510b4744dcbdf143413e8e74c33cc1b737fa559
Jon Rosenberg <jon_rosenberg@hotmail.com>
2022-06-06 23:01:56 -0700

    Initial build of Hero Manager

---
+++ Hero Manager/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Hero_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.reloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lvHeroes = new System.Windows.Forms.ListView();
+            this.chName = new System.Windows.Forms.ColumnHeader();
+            this.chStars = new System.Windows.Forms.ColumnHeader();
+            this.chLevel = new System.Windows.Forms.ColumnHeader();
+            this.chInVault = new System.Windows.Forms.ColumnHeader();
+            this.chLocked = new System.Windows.Forms.ColumnHeader();
+            this.chGearCount = new System.Windows.Forms.ColumnHeader();
+            this.chRequiredBooks = new System.Windows.Forms.ColumnHeader();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reloadToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1800, 38);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // reloadToolStripMenuItem
+            // 
+            this.reloadToolStripMenuItem.Name = "reloadToolStripMenuItem";
+            this.reloadToolStripMenuItem.Size = new System.Drawing.Size(94, 34);
+            this.reloadToolStripMenuItem.Text = "Reload";
+            this.reloadToolStripMenuItem.Click += new System.EventHandler(this.OnReloadHeroes);
+            // 
+            // lvHeroes
+            // 
+            this.lvHeroes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvHeroes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chStars,
+            this.chLevel,
+            this.chRequiredBooks,
+            this.chInVault,
+            this.chLocked,
+            this.chGearCount});
+            this.lvHeroes.HideSelection = false;
+            this.lvHeroes.Location = new System.Drawing.Point(12, 41);
+            this.lvHeroes.Name = "lvHeroes";
+            this.lvHeroes.Size = new System.Drawing.Size(1776, 1372);
+            this.lvHeroes.TabIndex = 1;
+            this.lvHeroes.UseCompatibleStateImageBehavior = false;
+            this.lvHeroes.View = System.Windows.Forms.View.Details;
+            this.lvHeroes.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.OnColumnClicked);
+            // 
+            // chName
+            // 
+            this.chName.Text = "Name";
+            this.chName.Width = 200;
+            // 
+            // chStars
+            // 
+            this.chStars.Text = "Stars";
+            // 
+            // chLevel
+            // 
+            this.chLevel.Text = "Level";
+            // 
+            // chInVault
+            // 
+            this.chInVault.Text = "In Vault?";
+            this.chInVault.Width = 100;
+            // 
+            // chLocked
+            // 
+            this.chLocked.Text = "Locked?";
+            this.chLocked.Width = 100;
+            // 
+            // chGearCount
+            // 
+            this.chGearCount.Text = "Gear Count";
+            this.chGearCount.Width = 125;
+            // 
+            // chRequiredBooks
+            // 
+            this.chRequiredBooks.Text = "Books Required";
+            this.chRequiredBooks.Width = 175;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1800, 1425);
+            this.Controls.Add(this.lvHeroes);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Hero Manager";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem reloadToolStripMenuItem;
+        private System.Windows.Forms.ListView lvHeroes;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chStars;
+        private System.Windows.Forms.ColumnHeader chLevel;
+        private System.Windows.Forms.ColumnHeader chInVault;
+        private System.Windows.Forms.ColumnHeader chLocked;
+        private System.Windows.Forms.ColumnHeader chGearCount;
+        private System.Windows.Forms.ColumnHeader chRequiredBooks;
+    }
+}
---
+++ Hero Manager/Form1.cs
@@ -0,0 +1,108 @@
+﻿using Raid.Client;
+using Raid.DataModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hero_Manager
+{
+    public partial class Form1 : Form
+    {
+        private RaidToolkitClient api;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.api = new RaidToolkitClient();
+            this.api.Connect();
+        }
+
+        private async void OnReloadHeroes(object sender, EventArgs e)
+        {
+            this.lvHeroes.Items.Clear();
+            var accounts = await api.AccountApi.GetAccounts();
+            var skillData = await api.StaticDataApi.GetSkillData();
+            var accountId = accounts.First().Id;
+            this.Text = string.Format("{0}'s Heroes", accounts.First().Name);
+            foreach (var hero in await api.AccountApi.GetHeroes(accountId))
+            {
+                // name, stars, level, books required, in vault?, locked?, gear count
+                ListViewItem item = new ListViewItem(hero.Name);
+                item.SubItems.Add(hero.Rank.Substring(5));
+                item.SubItems.Add(hero.Level.ToString());
+                item.SubItems.Add(CountBooksRequired(hero, skillData).ToString());
+                item.SubItems.Add(hero.InVault ? "X" : string.Empty);
+                item.SubItems.Add(hero.Locked ? "X" : String.Empty);
+                item.SubItems.Add(hero.EquippedArtifactIds.Count.ToString());
+                this.lvHeroes.Items.Add(item);
+            }
+        }
+
+        private static int CountBooksRequired(Hero hero, StaticSkillData skillData)
+        {
+            int booksRequired = 0;
+            foreach (var skill in hero.SkillsById.Values)
+            {
+                int booksNeededForSkill = skillData.SkillTypes[skill.TypeId].Upgrades.Count();
+                int booksAppliedToSkill = skill.Level - 1;
+                booksRequired += (booksNeededForSkill - booksAppliedToSkill);
+            }
+
+            return booksRequired;
+        }
+
+        private void OnColumnClicked(object sender, ColumnClickEventArgs e)
+        {
+            this.lvHeroes.ListViewItemSorter = HeroSorter.Get(e.Column, this.lvHeroes.ListViewItemSorter as HeroSorter);
+        }
+
+        #region Sorter
+        private class HeroSorter : IComparer
+        {
+            private int col;
+            private bool ascending;
+
+            private HeroSorter(int col, bool ascending)
+            {
+                this.col = col;
+                this.ascending = ascending;
+            }
+
+            public static HeroSorter Get(int col, HeroSorter prev)
+            {
+                if (prev != null && prev.col == col)
+                {
+                    return new HeroSorter(col, !prev.ascending);
+                }
+
+                return new HeroSorter(col, true);
+            }
+
+            public int Compare(object? x, object? y)
+            {
+                ListViewItem? a = x as ListViewItem;
+                ListViewItem? b = y as ListViewItem;
+
+                string aText = a != null ? a.SubItems[this.col].Text : String.Empty;
+                string bText = b != null ? b.SubItems[this.col].Text : String.Empty;
+
+                if (this.ascending)
+                {
+                    return aText.CompareTo(bText);
+                }
+                else
+                {
+                    return bText.CompareTo(aText);
+                }
+            }
+        }
+        #endregion
+    }
+}
